8c050c801dd121e1c00ea68813e9722226ed9134
Hamza <hamzajaved091@gmail.com>
2022-02-13 17:42:37 +0500

    UI Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState, useContext, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 import { ProductsContext } from "./context/ProductsContext";
 import InputForm from "./components/InputForm";
-
-import { saveAs } from "file-saver";
 
 function App() {
   const { connectWallet, currentAccount, productIDforQR } =
@@ -21,42 +17,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          {!currentAccount && (
-            <button type="button" onClick={connectWallet}>
+        <div>
+          {!currentAccount ? (
+            <button className="connect" type="button" onClick={connectWallet}>
               Connect to Wallet
             </button>
+          ) : (
+            <InputForm />
           )}
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <InputForm />
-        <div>
-          {productIDforQR ? (
-            <button onClick={downloadQR}>Download QR code</button>
-          ) : null}
         </div>
       </header>
     </div>
